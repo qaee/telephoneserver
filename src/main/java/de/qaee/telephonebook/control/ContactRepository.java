fab4d682a87388f7e67f8652f88c7e6bd5a6fa81
Qasim Ali <ranaqasim@gmail.com>
2018-04-30 11:01:52 +0200

    switched to spring boot

--- src/main/java/de/qaee/telephonebook/control/ContactRepository.java
+++ src/main/java/de/qaee/telephonebook/control/ContactRepository.java
@@ -3,7 +3,9 @@ package de.qaee.telephonebook.control;
 import de.qaee.telephonebook.model.Contact;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-@RepositoryRestResource(collectionResourceRel = "contacts", path = "contact")
+@RepositoryRestResource(collectionResourceRel = "contacts", path = "contacts")
+@CrossOrigin( origins = "*")
 public interface ContactRepository extends CrudRepository<Contact, Integer> {
 }
